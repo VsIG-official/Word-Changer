1fd806a377f2222caf6019ec344e3ea29a51677b
VsIG <Valentyn2202@gmail.com>
2020-06-14 12:10:34 +0300

    b(code) code [Main file]

---
+++ WordChanger/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Office.Interop.Word;
+
+namespace WordChanger
+{
+	class Program
+	{
+        static void Main(string[] args)
+        {
+            Application app = new Application();
+            Document doc = app.Documents.Add(Visible: true);
+            Range r = doc.Range();
+            r.Text = "Hello world";
+            int docLength = r.Text.Length - 1;
+            r.Text = docLength.ToString();
+            Random random = new Random();
+            doc.Save();
+            app.Documents.Open(@"C:\Users\Valentin\Desktop\ForFormatting.docx");
+            Console.ReadKey();
+
+			try
+			{
+                doc.Close();
+                app.Quit();
+			}
+			catch (Exception e)
+			{
+                Console.WriteLine(e.Message);
+			}
+
+			for (int i = 0; i < docLength; i++)
+			{
+                Console.WriteLine("Line");
+			}
+            Console.ReadKey();
+        }
+    }
+}
