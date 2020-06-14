bb58ed4514cbdbfe9483b72abd09380b1fa75b97
VsIG <Valentyn2202@gmail.com>
2020-06-14 20:08:15 +0300

    q(style) delete useless [Main file]

--- WordChanger/WordChanger.cs
+++ WordChanger/WordChanger.cs
@@ -5,9 +5,15 @@ using Microsoft.Office.Interop.Word;
 
 namespace WordChanger
 {
-	class Program
-	{
-        static void Main()
+	/// <summary>
+	/// Main class (hope its singletone)
+	/// </summary>
+	class WordChanger
+    {
+		/// <summary>
+		/// Defines the entry point of the application.
+		/// </summary>
+		static void Main()
         {
             Application app = new Application();
             Document doc = app.Documents.Add(Visible: true);
@@ -19,18 +25,14 @@ namespace WordChanger
             string fourthFont = "times new roman";
             r.Text = phrase;
             int docLength = r.Text.Length - 1;
-            //r.Text = docLength.ToString();
             char[] letters = phrase.ToCharArray();
-            //r.Text = phrase;
             r.Font.Size = 14;
             Random random = new Random();
 
             for (int i = 0; i < docLength; i++)
 			{
                 Range tempR = doc.Range(i, docLength);
-
                 int number = random.Next(4);
-
                 if (number == 1)
                 {
                     tempR.Font.Name = firstFont;
@@ -51,18 +53,6 @@ namespace WordChanger
 
             doc.Save();
             app.Documents.Open(@"C:\Users\Valentin\Desktop\ForFormatting.docx");
-            Console.ReadKey();
-
-			try
-			{
-                doc.Close();
-                app.Quit();
-			}
-			catch (Exception e)
-			{
-                Console.WriteLine(e.Message);
-			}
-
             Console.ReadKey();
         }
     }
