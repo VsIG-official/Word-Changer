9bed327267677d57ec341eaf99b7a20a6efd69cb
VsIG <Valentyn2202@gmail.com>
2020-10-08 14:50:54 +0300

    📝: add some comments

--- WordChanger/WordChanger.cs
+++ WordChanger/WordChanger.cs
@@ -10,9 +10,12 @@ namespace WordChanger
 	/// </summary>
 	class WordChanger
     {
+        // Path to file, from which You want to read
 		private const string ReadFrom = @"YourDisk:\Users\YourUser\Desktop\ReadFrom.txt";
+        // Path to file, where You want to save Your text with different fonts
         private const string ChangeIn = @"YourDisk:\Users\YourUser\Desktop\ChangeIn.docx";
 
+        // All possible fonts and their count
         const int numberOfFonts = 4;
         const string FirstFont = "arial";
         const string SecondFont = "verdana";
@@ -21,6 +24,7 @@ namespace WordChanger
 
 		static readonly string[] Fonts = { FirstFont, SecondFont, ThirdFont, FourthFont};
 
+        // Size of a text
         const int TextSize = 14;
 
         /// <summary>
@@ -28,27 +32,37 @@ namespace WordChanger
         /// </summary>
         static void Main()
         {
+            // Create range of .docx file
             Application app = new Application();
             Document doc = app.Documents.Add(Visible: true);
             Range rangeOfDoc = doc.Range();
 
+            // Get the text from .txt file and set text of doc to it
             string phrase = File.ReadAllText(ReadFrom, Encoding.Default);
             rangeOfDoc.Text = phrase;
 
+            // Count length of text (in characters)
             int docLength = rangeOfDoc.Text.Length - 1;
+            // Put all chars to an array
             char[] letters = phrase.ToCharArray();
+            // Set text to a specific size
             rangeOfDoc.Font.Size = TextSize;
 
+            // Create new random
             Random random = new Random();
 
+            // Iterate through each character
             for (int i = 0; i < docLength; i++)
 			{
                 Range tempR = doc.Range(i, docLength);
+                // Generate random number
                 int number = random.Next(numberOfFonts);
 
+                // Set the random font to letter
                 tempR.Font.Name = Fonts[number];
             }
 
+            // Save the .docx and open it
             doc.Save();
             app.Documents.Open(ChangeIn);
             Console.ReadKey();
