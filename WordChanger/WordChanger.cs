cd6432ec5b503113c35cd5e8bb88cf27b2e38eb0
VsIG <Valentyn2202@gmail.com>
2020-10-08 13:01:44 +0300

    ♻️: refactor magic numbers

--- WordChanger/WordChanger.cs
+++ WordChanger/WordChanger.cs
@@ -13,6 +13,15 @@ namespace WordChanger
 		private const string readFrom = @"C:\Users\Valentin\Desktop\readFrom.txt";
         private const string changeIn = @"C:\Users\Valentin\Desktop\changeIn.docx";
 
+        static string FirstFont = "arial";
+        static string SecondFont = "verdana";
+        static string ThirdFont = "arial black";
+        static string FourthFont = "times new roman";
+
+		static string[] Fonts = { FirstFont, SecondFont, ThirdFont, FourthFont};
+
+        const int TextSize = 14;
+
         /// <summary>
         /// Defines the entry point of the application.
         /// </summary>
@@ -20,38 +29,23 @@ namespace WordChanger
         {
             Application app = new Application();
             Document doc = app.Documents.Add(Visible: true);
-            Range r = doc.Range();
+            Range rangeOfDoc = doc.Range();
+
             string phrase = File.ReadAllText(readFrom, Encoding.Default);
-            string firstFont = "arial";
-            string secondFont = "verdana";
-            string thirdFont = "arial black";
-            string fourthFont = "times new roman";
-            r.Text = phrase;
-            int docLength = r.Text.Length - 1;
+            rangeOfDoc.Text = phrase;
+
+            int docLength = rangeOfDoc.Text.Length - 1;
             char[] letters = phrase.ToCharArray();
-            r.Font.Size = 14;
+            rangeOfDoc.Font.Size = TextSize;
+
             Random random = new Random();
 
             for (int i = 0; i < docLength; i++)
 			{
                 Range tempR = doc.Range(i, docLength);
                 int number = random.Next(4);
-                if (number == 1)
-                {
-                    tempR.Font.Name = firstFont;
-                }
-                else if(number == 2)
-                {
-                    tempR.Font.Name = secondFont;
-                }
-                else if (number == 3)
-                {
-                    tempR.Font.Name = thirdFont;
-                }
-				else
-				{
-                    tempR.Font.Name = fourthFont;
-                }
+
+                tempR.Font.Name = Fonts[number];
             }
 
             doc.Save();
